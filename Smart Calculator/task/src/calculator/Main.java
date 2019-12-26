b22c20b638fab77429ce65f8617bb1ca207801c0
Zeegit <sergey.zemskov@gmail.com>
2019-12-26 12:32:54 +0300

    stage 4

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,15 +1,37 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
+        String in;
+        while (true) {
+            in = scanner.nextLine()
+                    .replaceAll("\\s+", "")
+                    .replaceAll("-{2}", "+")
+                    //.replaceAll("--", "+")
+                    .replaceAll("\\+{1,}", "+")
+                    // .replaceAll("\\+-", "-")
+                    .replaceAll("-", "+-");
+            if ("/exit".equals(in)) {
+                break;
+            } else if ("/help".equals(in)) {
+                System.out.println("The program calculates the sum of numbers");
+            } else if (!in.isEmpty()) {
+                int sum = 0;
+                for(String s: in.split("\\+")) {
+                    if (!s.isEmpty()) {
+                        sum += Integer.parseInt(s);
+                    }
+                }
+                System.out.println(sum);
+
+            }
+        }
+        System.out.println("Bye!");
     }
 }
